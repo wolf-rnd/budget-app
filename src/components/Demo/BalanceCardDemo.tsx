56e80b1fd8495dffc3df7a16e8a340d66e801952
wolf user <159774722+wolf-rnd@users.noreply.github.com>
2025-06-13 04:50:18 +0300

    Updated BalanceCardDemo.tsx

--- src/components/Demo/BalanceCardDemo.tsx
+++ src/components/Demo/BalanceCardDemo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CreditCard, TrendingUp, DollarSign } from 'lucide-react';
+import { CreditCard, DollarSign } from 'lucide-react';
 
 const BalanceCardDemo: React.FC = () => {
   const formatCurrency = (amount: number) => {
